068f1c62c0f1b8ed461950ca0f54f9bc74fff4ca
Rahul Prajapati <rahul.prajapati1127@gmail.com>
2024-03-22 16:03:44 +0530

    Create Header, Introduction & About Section

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Homepage';
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-       Hello World
-      </h1>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
