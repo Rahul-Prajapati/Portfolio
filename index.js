5ca1bccfc49e0885534d0a45e5ba47645a4a7493
Rahul Prajapati <rahul.prajapati1127@gmail.com>
2024-04-29 11:38:30 +0530

    Deployment code added

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from 'express';
 import mongoose from  'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
-
 
  dotenv.config();
 import portfolioRoute from './server/routes/portfolioRoute.js';
@@ -32,3 +32,9 @@ app.listen(4000, () => {
     console.log('Server is running on port 4000!');
     }
 )
+
+const __dirname = path.resolve(); 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
